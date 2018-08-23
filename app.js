76b97cb002125931443e8207b2764c30d4662e88
aramos <aramos@miamidade.gov>
2018-08-23 15:07:17 -0400

    removed lines

--- app.js
+++ app.js
@@ -36,8 +36,6 @@ var command = argv._[0];
 //console.log('Yargs', argv)
 
 
-
-
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
     if(_.isUndefined(note) === false){
